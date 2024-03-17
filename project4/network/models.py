b329e0de859149118cea4d3286cfa73172bbba0c
Raza Hanif <raza_hanif@ymail.com>
2024-03-17 15:30:59 -0400

    finished first draft of models

--- project4/network/models.py
+++ project4/network/models.py
@@ -3,30 +3,33 @@ from django.db import models
 
 # For debugging all default strings use all variables
 
+# Users inherit from AbstractUser, can have a username of min 4 char max 15
 class User(AbstractUser):
     # Min username length should be 4 but idk how to do that rn
     username = models.CharField(max_length=15)
-    # Followers and Following handled in its own model
     
     def __str__(self):
         return f"User {self.id}: {self.username}"
 
+# Posts contain content, date created, who created it, and if it has been edited or not
 class Post(models.Model):
     content = models.TextField()
     date = models.DateField()
-    edited = models.BooleanField()
+    edited = models.BooleanField(default=False)
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="poster")
     
     def __str__(self):
         return f"{self.user}: {self.content} @ {self.date} | {self.edited}"
     
+# Which user likes which post
 class Likes(models.Model):
     post = models.ForeignKey("Posts", on_delete=models.CASCADE, related_name="post")
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user")
     
     def __str__(self):
-        return f"{self.user} likes post -- {self.post}"    
-    
+        return f"{self.user} likes post -- {self.post}" 
+       
+# Large Table of comments pointing to users and posts
 class Comment(models.Model):
     comment = models.TextField()
     post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="post")
