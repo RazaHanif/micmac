6aaf2c6c5c91d5be43d4d4ca47f6ade2c523a0a8
Raza Hanif <raza_hanif@ymail.com>
2025-02-17 14:26:13 -0500

    starting today, goal is to just get the Mainp

--- project4/network-react-frontend/src/pages/MainPage.jsx
+++ project4/network-react-frontend/src/pages/MainPage.jsx
@@ -1,7 +1,12 @@
+import React, { useState } from "react"
 import Card from "./Card"
 
 function MainPage() {
+    // Work on making this card talk to api and get info
+    // Break down Card into smaller chunks and fill in info with props
+    // figrue out how to render multiple Cards within loop
 
+    
 
     return(
         <>
