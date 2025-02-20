33c365168e8ae2af849a55b662c9c6b81e160a28
Raza Hanif <raza_hanif@ymail.com>
2025-02-19 23:41:59 -0500

    tbh idek

--- project4/network-react-frontend/src/pages/Card.jsx
+++ project4/network-react-frontend/src/pages/Card.jsx
@@ -1,32 +1,33 @@
 import React, { useState, useEffect } from "react";
+import PropTypes from 'prop-types'
 
-function Card(props) {
+function Card({ user, content, date, edited, postId}) {
     // Placeholder for user image - i dont have backend func for this
     const imgSrc = "https://cdn.jsdelivr.net/npm/twemoji@11.3.0/2/svg/1f9f1.svg"
 
     // deconstruct props and declare inital vars
-    const [user, setUser] = useState(props.user)
-    const [content, setContent] = useState(props.content)
-    const [date, setDate] = useState(props.date)
-    const [edited, setEdited] = useState(props.edited)
-    const [id, setId] = useState(props.id)
+    const [userState, setUserState] = useState(user)
+    const [contentState, setContentState] = useState(content)
+    const [dateState, setDateState] = useState(date)
+    const [editedState, setEditedState] = useState(edited)
+    const [postIdState, setPostIdState] = useState(postId)
 
     // Checks if post was made today, if made today will change date to 'today'
     useEffect(() => {
         let today = new Date()
         today = today.toISOString().split('T')[0]
 
-        if (props.date == today){
+        if (dateState == today){
             setDate("today")
         }
         else {
-            setDate(props.date)
+            setDate(date)
         }
     }, [])
 
     // Used to get the username for each post
     useEffect(() => {
-        const url = 'http://127.0.0.1:8000/user/' + props.user
+        const url = 'http://127.0.0.1:8000/user/' + userState
 
         fetch(url, {
             method: 'GET',
@@ -44,28 +45,40 @@ function Card(props) {
 
     
     return(
-        <>
-            <div className="card">
-                <div className="card-user">
-                    <div className="card-user-pic">
-                        <img src={imgSrc} alt="profile picture"/>
-                    </div>
-                    <div className="card-user-name">
-                        {user.username}
-                    </div>
+        <div className="card">
+            <div className="card-user">
+                <div className="card-user-pic">
+                    <img src={imgSrc} alt="profile"/>
+                </div>
+                <div className="card-user-name">
+                    {userState.username}
+                </div>
 
-                </div>
-                <div className="card-post">
-                    <div className="card-text">
-                        <p className="card-date">{date}</p>
-                        <h6 className="card-content">{content}</h6>
-                        <p className="card-edited-flag">{edited ? 'edited' : ""}</p>
-                    </div>
-                </div>
-                
             </div>
-        </>
+            <div className="card-post">
+                <div className="card-text">
+                    <p className="card-date">{dateState}</p>
+                    <h6 className="card-content">{contentState}</h6>
+                    <p className="card-edited-flag">{editedState ? 'eEditedState' : ""}</p>
+     postIdState     PostIdState</div>
+            </div>
+            
+        </div>
     );
+}
+
+PropCard.propTypes = {
+    name: PropTypes.string,
+    date: PropTypes.string,
+    id: PropTypes.number,
+    user: PropTypes.number,
+    edited: PropTypes.bool,
+}
+
+PropCard.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false,
 }
 
 export default Card
