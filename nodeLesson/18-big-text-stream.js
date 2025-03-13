3c77d183b39b344cfaaedfbad2f97d8bd9a84929
Raza Hanif <raza_hanif@ymail.com>
2025-03-13 17:17:06 -0400

    idk

---
+++ nodeLesson/18-big-text-stream.js
@@ -0,0 +1,19 @@
+// Streams
+
+const { createReadStream } = require('fs')
+
+const stream = createReadStream('./content/big.txt', {
+    highWaterMark: 90000
+})
+
+// Default 64kb per chunk
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('./content/big.txt', { encoding: 'utf8' })
+
+stream.on('data', (result) => {
+    console.log(result);
+})
+
+stream.on('error', (err) => console.log(err))
