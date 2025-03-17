84695a71bc36655f61dff71785e6f915601eae9f
Raza Hanif <raza_hanif@ymail.com>
2025-03-17 16:58:31 -0400

    done for now, completed until methods-get

--- nodeLesson/02-express/06-middleware.js
+++ nodeLesson/02-express/06-middleware.js
@@ -3,12 +3,19 @@
 
 const express = require('express')
 const app = express()
-const logger = require('./logger.js')
-const authorize = require('./auth.js')
+// const logger = require('./logger.js')
+// const authorize = require('./auth.js')
+const morgan = require('morgan')
 
 // applies the middleware to all routes
 // needs to be at the very top of the routes
-app.use(logger, authorize)
+// app.use(logger, authorize)
+
+
+// 1. use vs route
+// 2. options - our own / express / third party
+app.use(morgan('tiny'))
+
 
 app.get('/', (req, res) => {
     console.log(req);
