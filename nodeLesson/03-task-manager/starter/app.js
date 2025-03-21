0a90c816ff657160c596469f2c9f2ae316cb13e5
Raza Hanif <raza_hanif@ymail.com>
2025-03-21 19:20:59 -0400

    i think im done for today

--- nodeLesson/03-task-manager/starter/app.js
+++ nodeLesson/03-task-manager/starter/app.js
@@ -8,14 +8,11 @@ const tasks = require('./routes/tasks.js')
 
 const port = 3000
 
+app.use(express.static('./public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
 
 app.use('/api/v1/tasks', tasks)
-
-app.get('/hello', (req, res) => {
-    res.status(200).send(`Hello, World!`)
-})
 
 
 const start = async () => {
--- nodeLesson/03-task-manager/starter/controller/tasks.js
+++ nodeLesson/03-task-manager/starter/controller/tasks.js
@@ -2,8 +2,8 @@ const Task = require('../models/Task.js')
 
 const getTasks = async (req, res) => {
     try {
-        const allTasks = await Task.find({})
-        res.status(200).json({ allTasks })
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
     } catch (err) {
         return res.status(500).send(err)
     }
@@ -11,8 +11,8 @@ const getTasks = async (req, res) => {
 
 const createTask = async (req, res) => {
     try {
-        const newTask = await Task.create(req.body)
-        res.status(200).json({ newTask })
+        const task = await Task.create(req.body)
+        res.status(200).json({ task })
     } catch (err) {
         return res.status(500).send(err)
     }
