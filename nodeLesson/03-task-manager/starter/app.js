f3633833bb414f6ca102b8135f5bdacd4d3f1ad2
Raza Hanif <raza_hanif@ymail.com>
2025-03-21 19:33:02 -0400

    done for the day

--- nodeLesson/03-task-manager/starter/app.js
+++ nodeLesson/03-task-manager/starter/app.js
@@ -2,17 +2,18 @@ const express = require('express')
 const app = express()
 const connectDB = require('./db/connect.js')
 require('dotenv').config()
-
-
 const tasks = require('./routes/tasks.js')
-
 const port = 3000
+const notFound = require('./middleware/not-found.js')
 
+// middleware
 app.use(express.static('./public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
 
+// routes
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
 
 
 const start = async () => {
