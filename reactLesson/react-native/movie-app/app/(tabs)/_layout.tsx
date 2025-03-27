40401914bc4ba695e86d04de579d064daa277249
Raza Hanif <raza_hanif@ymail.com>
2025-03-27 16:12:07 -0400

    Learning React Native @ 4:12:06 PM

--- reactLesson/react-native/movie-app/app/(tabs)/_layout.tsx
+++ reactLesson/react-native/movie-app/app/(tabs)/_layout.tsx
@@ -9,7 +9,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
         return (
             <ImageBackground
                 source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
             >
                 <Image 
                     source={icon} 
@@ -48,7 +48,15 @@ const _Layout = () => {
                 alignItems: 'center'
             },
             tabBarStyle: {
-                backgroundColor: '#0F0D23'
+                backgroundColor: '#0F0D23',
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: 'absolute',
+                overflow: 'hidden',
+                borderWidth: 1,
+                borderColor: '#0F0D23'
             }
         }}
     >
