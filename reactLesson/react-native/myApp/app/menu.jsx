08dc5ef689bbc6ab0133d582d4842daf5c466a5c
Raza Hanif <raza_hanif@ymail.com>
2025-03-28 09:21:44 -0400

    Learning React Native @ 9:21:44 AM

--- reactLesson/react-native/myApp/app/menu.jsx
+++ reactLesson/react-native/myApp/app/menu.jsx
@@ -1,6 +1,8 @@
-import { View, Text, StyleSheet, Appearance, Platform, SafeAreaView, FlatList } from 'react-native'
-import { Colors } from '@/constants/Colors'
+import { View, Text, Image, StyleSheet, Appearance, Platform, SafeAreaView, FlatList } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
+import { Colors } from '@/constants/Colors'
+import { MENU_ITEMS } from '@/constants/MenuItems'
+import MENU_IMAGES from '@/constants/MenuImages'
 
 const MenuScreen = () => {
     const colorScheme = Appearance.getColorScheme()
@@ -14,8 +16,25 @@ const MenuScreen = () => {
   return (
     <Container>
         <FlatList
-            data={[]}
-            renderItem={({ item }) => {}}
+            data={MENU_ITEMS}
+            keyExtractor={(item) => item.id.toString()}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+                <View style={styles.card}>
+                    <View style={styles.text}>
+                        <Text style={styles.title}>
+                            { item.title }
+                        </Text>
+                        <Text style={styles.desc}>
+                            { item.description }
+                        </Text>
+                    </View>
+                    <Image
+                        source={MENU_IMAGES[item.id - 1]}
+                        style={styles.img}
+                    />
+                </View>
+            )}
         />
     </Container>
   )
@@ -23,7 +42,33 @@ const MenuScreen = () => {
 
 function createStyles(theme, colorScheme) {
     return StyleSheet.create({
-
+        card: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: 10,
+            padding: 10,
+            backgroundColor: 'grey',
+            borderRadius: 10
+        },
+        text: {
+            paddingBottom: 20
+        },
+        title: {
+            fontSize: 24,
+            fontWeight: 'bold',
+            textAlign: 'center'
+        },
+        desc: {
+            fontSize: 14,
+            textAlign: 'center'
+        },
+        img: {
+            width: 150,
+            height: 150,
+            resizeMode: 'stretch'
+        },
     })
 }
 
